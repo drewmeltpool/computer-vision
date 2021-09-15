23fec438faca7e68b1c8204bea08cbdc724c2e0e
drewmeltpool <drewmelt132@gmail.com>
2021-09-15 14:35:04 +0300

    feature: complete 2 task

--- graphics-basic/src/main.py
+++ graphics-basic/src/main.py
@@ -1,4 +1,5 @@
-import turtle;
+import turtle
+import math
 from shapes.square import square
 from shapes.circle import circle
 from utils.shapeGroup import shapeGroup
@@ -10,8 +11,8 @@ circleGroup = shapeGroup(circle)
 
 squareGroup(
     [-200, 0],
-    20,
-    10,
+    50,
+    5,
     lambda s, i: -(s * i) / 2,
     lambda shape, offset, side, i, p:
         shape(side * i, p + offset[0], p + offset[1])
@@ -19,11 +20,43 @@ squareGroup(
 
 circleGroup(
     [200, 0],
-    20,
-    10,
+    25,
+    5,
     lambda s, i: -(s * i) / 2,
     lambda shape, offset, side, i, p:
-        shape(side * i / 2, offset[0], (-side * i / 2) + offset[1])
+        shape(side * i, offset[0], (-side * i) + offset[1])
 )
+
+# def createShape():
+#     return turtle.Turtle()
+#
+# def createPolygon(shape, lines, cb):
+#     cb(shape)
+#     for line in lines:
+#         shape.forward(line[0])
+#         shape.right(line[1])
+#     return shape
+#
+# def fill(shape, color, cb):
+#     shape.color(color, color)
+#     shape.begin_fill()
+#     cb(shape)
+#     shape.end_fill()
+#     return shape
+#
+# def polygon(lines, color, cb):
+#     return fill(createShape(), color, lambda s: createPolygon(s, lines, cb))
+#
+#
+# side = 200
+# radius = side * (math.sqrt(4 + 2 * math.sqrt(2)) / 2)
+#
+# def rotate(shape, angle):
+#     shape.right(angle)
+#
+# polygon([[radius, 112.5], [side, 112.5], [radius, 135]], 'red', lambda s: rotate(s, 0))
+# polygon([[radius, 112.5], [side, 112.5], [radius, 135]], 'green', lambda s: rotate(s, 90))
+# polygon([[radius, 112.5], [side, 112.5], [radius, 135]], 'blue', lambda s: rotate(s, 180))
+# polygon([[radius, 112.5], [side, 112.5], [radius, 135]], 'cyan', lambda s: rotate(s, 270))
 
 window.mainloop()
